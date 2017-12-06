996064e08f101de853fd137130ec28a4dfd4d3e3
Yannick van Leeuwen <yannickvanleeuwen@i-lion.nl>
2017-12-06 09:55:53 +0100

    designer update

--- ReceiveLogData/ReceiveLogData/Form1.Designer.cs
+++ ReceiveLogData/ReceiveLogData/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(945, 477);
+            this.ClientSize = new System.Drawing.Size(945, 306);
             this.Controls.Add(this.buttonGetFromQueue);
             this.Controls.Add(this.dataFromQueue);
             this.Name = "GetDataFromQueue";
